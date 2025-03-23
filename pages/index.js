acd8aa6cafdb869f7992429084b03d037b69bb81
Taha Jasim Mohammed <59452235+taha162@users.noreply.github.com>
2025-03-23 23:08:31 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     title: seoData.openGraph.title,
     excerpt: 'home',
     slug: '/',
-    coverImage: 'https://vjy.me/preview.jpg'
+    coverImage: 'public/PhotoRoom-20250322_110724.png'
   };
   return (
     <>
