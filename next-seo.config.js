131b1b9cdae9dfca0e9561bf44fa25abf49587cb
Taha Jasim Mohammed <59452235+taha162@users.noreply.github.com>
2025-03-24 07:01:04 +0300

    Create next-seo.config.js

---
+++ next-seo.config.js
@@ -0,0 +1,20 @@
+export default {
+    openGraph: {
+        type: 'website',
+        locale: 'en_IE',
+        title: 'Click to see more info about Eng. Taha',
+        url: 'https://tjd162.vercel.app',
+        description: 'This is my official website',
+        locale: 'en_EN',
+        keywords: 'designer, product designer, illustrator, indian designer, vijay verma, realvjy, illlustrations, indian illustrator, ui designer india, sushi design system, zomato designer, 3dicons, uiprint, vijay realvjy, indian ui designer, design india, overlayz, nft art',
+        images: [
+            {
+                width: 1200,
+                height: 630,
+                url: `https://firebasestorage.googleapis.com/v0/b/messages-chat.appspot.com/o/avatar.png?alt=media&token=74e9b55b-29f4-4122-81f7-023f08631f0f`,
+            },
+        ],
+        site_name: 'Eng. Taha Jasim Mohammed',
+    },
+
+};
